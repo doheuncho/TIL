2327bcad89288c8b10bd464b47bb826b17307985
도흔 조 <dh30594@naver.com>
2021-12-21 08:38:32 +0900

    self study: HackerRank 2021-12-21

---
+++ HackerRank/3Month Preparation Kit/Week2_Grading_Students.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+
+def gradingStudents(grades):
+    # Write your code here
+    result = []
+    for grade in grades:
+        if grade < 36:
+            result.append(grade)
+        # 5 단위 반올림 method
+        elif grade%5 < 3:
+            result.append(grade)
+        else:
+            result.append(grade + 5 - grade%5)
+    
+    return result
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
