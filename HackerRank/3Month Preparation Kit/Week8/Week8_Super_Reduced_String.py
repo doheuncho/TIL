a06d2594f050bf964a3f01de5cc59195bc070ff7
Doheun <dh30594@gmail.com>
2022-02-05 11:45:54 +0900

    self study: HackerRank 2022-02-05

---
+++ HackerRank/3Month Preparation Kit/Week8/Week8_Super_Reduced_String.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+
+def superReducedString(s):
+    # Write your code here
+    stack = []
+    
+    for c in s:
+        if stack == []:
+            stack.append(c)
+        elif stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    
+    if stack:
+        return ''.join(stack)
+    else:
+        return 'Empty String' 
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
