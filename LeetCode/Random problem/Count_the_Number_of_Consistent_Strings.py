40e1788cfc0ce94751253d8c98d73b2eb19788c9
Doheun <dh30594@gmail.com>
2022-05-01 12:50:59 +0900

    Self study: LeetCode 1684

---
+++ LeetCode/Random problem/Count_the_Number_of_Consistent_Strings.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/count-the-number-of-consistent-strings/
+
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        result = 0
+        
+        for word in words:
+            consistent = True
+            for char in word:
+                if char not in allowed:
+                    consistent = False
+                    break
+            if consistent:
+                result += 1
+        
+        return result
+    
