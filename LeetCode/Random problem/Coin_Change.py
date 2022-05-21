c28a825ef05e0bf66337f626aca5f73c5d154e2c
Doheun <dh30594@gmail.com>
2022-05-21 11:39:57 +0900

    Self study: LeetCode 322

---
+++ LeetCode/Random problem/Coin_Change.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/coin-change/
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] + [float('inf') for _ in range(amount)]
+        
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+                    
+        if dp[-1] == float('inf'):
+            return -1
+        
+        return dp[-1]
+    
