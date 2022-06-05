793a0062d3973f9c204358b115b2e5edb069530f
Doheun <dh30594@gmail.com>
2022-06-05 11:59:08 +0900

    Self study: LeetCode 51

--- LeetCode/Random problem/N-Queens.py
+++ LeetCode/Random problem/N-Queens.py
@@ -2,7 +2,7 @@
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res = []
+        result = []
         
         col = set() 
         pos_diagonal = set()
@@ -13,23 +13,23 @@ class Solution:
         def solve(r):
             if r==n:
                 copy = ["".join(row) for row in board]
-                res.append(copy)
+                result.append(copy)
                 
             for c in range(n):
-                if c in col or r+c in pos_diagonal or r-c in neg_diagonal:
+                if c in col or r + c in pos_diagonal or r - c in neg_diagonal:
                     continue
                 
-                pos_diagonal.add(r+c)
-                neg_diagonal.add(r-c)
+                pos_diagonal.add(r + c)
+                neg_diagonal.add(r - c)
                 
                 col.add(c)
                 
                 board[r][c] = "Q"
                 
-                solve(r+1)
+                solve(r + 1)
                 
-                pos_diagonal.remove(r+c)
-                neg_diagonal.remove(r-c)
+                pos_diagonal.remove(r + c)
+                neg_diagonal.remove(r - c)
                 col.remove(c)
                 
                 board[r][c] = "."
@@ -37,4 +37,4 @@ class Solution:
         
         solve(0)
         
-        return res
+        return result
