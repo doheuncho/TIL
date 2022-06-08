84d700116642c3a3ffc54ee24f026724ba657608
Doheun <dh30594@gmail.com>
2022-06-08 22:14:25 +0900

    Self study: LeetCode 1332

---
+++ LeetCode/Random problem/Remove_Palindromic_Subsequences.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/remove-palindromic-subsequences/
+
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        if s == s[::-1]:
+            return 1
+        else:
+            return 2
+        
