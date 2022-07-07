60cdb2e3814fc0934eb4201c19ab94a96cfa9330
Doheun <dh30594@gmail.com>
2022-07-07 20:07:44 +0900

    Self study: LeetCode 97

---
+++ LeetCode/Weekly Special/Interleaving_String.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/interleaving-string/
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = [[True for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+        
+        for i in range(len(s1)+1):
+            for j in range(len(s2)+1):
+                if i == j == 0:
+                    continue
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                else:
+                    dp[i][j] = (dp[i][j-1] and s2[j-1] == s3[i+j-1]) or (dp[i-1][j] and s1[i-1] == s3[i+j-1])
+        
+        return dp[-1][-1]
+    
