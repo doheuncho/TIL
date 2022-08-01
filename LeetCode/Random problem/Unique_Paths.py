abffee6cc1acf29c1233f7d5df5e6aaddf1ebb0e
Doheun <dh30594@gmail.com>
2022-08-01 21:13:13 +0900

    Self study: LeetCode 62

---
+++ LeetCode/Random problem/Unique_Paths.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        
+        for col in range(1, m):
+            for row in range(1, n):
+                dp[col][row] = dp[col-1][row] + dp[col][row-1]
+        
+        return dp[-1][-1]
+    
