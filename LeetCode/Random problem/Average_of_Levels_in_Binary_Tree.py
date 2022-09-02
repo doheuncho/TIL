9542ba0bd19b939156fcdaa192b36ce120eb9989
Doheun <dh30594@gmail.com>
2022-09-02 20:44:52 +0900

    Self study: LeetCode 637

---
+++ LeetCode/Random problem/Average_of_Levels_in_Binary_Tree.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        result = []
+        queue = collections.deque([root])
+        
+        while queue:
+            level_sum = 0
+            level_nums = len(queue)
+            
+            for _ in range(level_nums):
+                node = queue.popleft()
+                level_sum += node.val
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(level_sum/level_nums)
+        
+        return result
+                    
