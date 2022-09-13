72e7bf692b93e9578fe5dd7c3afe8e378aaa3479
Doheun <dh30594@gmail.com>
2022-09-13 14:46:56 +0900

    Self study: LeetCode 2022-09-2nd

---
+++ LeetCode/Weekly Special/SEP_Week2_Design_Hit_Counter.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/design-hit-counter/
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
+class HitCounter:
+    def __init__(self):
+        self.deque = collections.deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.deque.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.deque and timestamp - self.deque[0] >= 300:
+            self.deque.popleft()
+        return len(self.deque)
