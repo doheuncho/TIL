b2ed8017a0b3682e8bf932a3def0b8fb736cd52f
Doheun <dh30594@gmail.com>
2022-09-20 12:49:24 +0900

    Self study: LeetCode 718

---
+++ LeetCode/Random problem/Maximum_Length_of_Repeated_Subarray.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0 for _ in range(len(nums2) + 1)] for _ in range(len(nums1) + 1)]
+        
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1
+        
+        return max(max(dp[i]) for i in range(len(nums1) + 1))
+    
