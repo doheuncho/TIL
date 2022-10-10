c3fb4bea24415d2684a52d0c829ec63a73b7e00a
Doheun <dh30594@gmail.com>
2022-10-10 14:43:05 +0900

    Self study: LeetCode 1328

---
+++ LeetCode/Random problem/Break_a_Palindrome.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/break-a-palindrome/
+
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        n = len(palindrome)
+        if n == 1:
+            return ""
+        
+        for i in range(n // 2):
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i+1:]
+        
+        return palindrome[:-1] + 'b'
+    
