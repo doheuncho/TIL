04d9f6eda408e20ca86d837b19729b4bf2351506
Doheun <dh30594@gmail.com>
2022-10-21 22:31:59 +0900

    Self study: LeetCode 219

---
+++ LeetCode/Random problem/Contains_Duplicate_II.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic = {}
+        
+        for i in range(len(nums)):
+            if nums[i] in dic and i - dic[nums[i]] <= k:
+                return True
+            dic[nums[i]] = i
+        
+        return False
