10e7574f41ea9074240ab217c1055c79f50ab474
Doheun <dh30594@gmail.com>
2022-12-05 20:24:49 +0900

    Self study: LeetCode 876

---
+++ LeetCode/Random problem/Middle_of_the_Linked_List.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = mid = head
+        
+        while cur and cur.next:
+            mid = mid.next
+            cur  = cur.next.next
+        
+        return mid
+        
