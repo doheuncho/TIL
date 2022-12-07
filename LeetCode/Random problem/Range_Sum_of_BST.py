1d92beaa3c4c7eb8ee1939f5555daa714195b816
Doheun <dh30594@gmail.com>
2022-12-07 20:56:51 +0900

    Self study: LeetCode 938

---
+++ LeetCode/Random problem/Range_Sum_of_BST.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/range-sum-of-bst/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        stack, result = [root], 0
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+                if low <= node.val <= high:
+                    result += node.val
+        
+        return result
