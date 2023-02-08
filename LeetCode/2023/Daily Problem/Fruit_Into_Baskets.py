c99c9f88d22666273dfc6ab50b8a1da95f3ebab5
doheuncho <dh30594@gmail.com>
2023-02-08 09:32:57 +0900

    Self study: LeetCode 904

---
+++ LeetCode/2023/Daily Problem/Fruit_Into_Baskets.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {}
+        left = 0
+        
+        for right, fruit in enumerate(fruits):
+            basket[fruit] = basket.get(fruit, 0) + 1
+
+            if len(basket) > 2:
+                basket[fruits[left]] -= 1
+
+                if basket[fruits[left]] == 0:
+                    basket.pop(fruits[left])
+                left += 1
+        
+        return right - left + 1
+        
