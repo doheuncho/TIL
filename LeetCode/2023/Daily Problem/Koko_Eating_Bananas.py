2a3646fe97c215f4d59966de27182be0b0e87e9e
doheuncho <dh30594@gmail.com>
2023-03-08 20:44:25 +0900

    Self study: LeetCode 875

---
+++ LeetCode/2023/Daily Problem/Koko_Eating_Bananas.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+
+        while left < right:
+            mid = (left + right) // 2
+            if sum((p + mid - 1) // mid for p in piles) <= h:
+                right = mid
+            else:
+                left = mid + 1
+
+        return right
