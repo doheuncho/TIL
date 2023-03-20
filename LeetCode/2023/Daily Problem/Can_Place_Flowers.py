e08f55748b10004d147122522f22731eaadb0593
doheuncho <dh30594@gmail.com>
2023-03-20 22:17:58 +0900

    Self study: LeetCode 605

---
+++ LeetCode/2023/Daily Problem/Can_Place_Flowers.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/can-place-flowers/
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        
+        for i in range(1, len(flowerbed) - 1):
+           if flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] == 0:
+               flowerbed[i] = 1
+               n -= 1
+
+        return n <= 0
+    
