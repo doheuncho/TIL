686dc10c0a99132d9d72da9258791e7a683a728d
doheuncho <dh30594@gmail.com>
2023-03-31 21:36:50 +0900

    Self study: LeetCode 1236

---
+++ LeetCode/2023/Weekly Problem/MAR_Week4_Web_Crawler.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/web-crawler/
+
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        queue, result = [startUrl], set([startUrl])
+        name = startUrl.split('/')[2]
+
+        while queue:
+            url = queue.pop(0)
+            for x in htmlParser.getUrls(url):
+                if x not in result and name == x.split('/')[2]:
+                    queue.append(x)
+                    result.add(x)
+
+        return list(result)
