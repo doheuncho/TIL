a066c593e28e06e4f9f3f107900216bcf4f6828a
doheuncho <dh30594@gmail.com>
2023-04-01 20:03:38 +0900

    Self study: LeetCode 2023-04-1st

---
+++ LeetCode/2023/Weekly Problem/APR_Week1_Shortest_Word_Distance_III.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/shortest-word-distance-iii/
+
+class Solution:
+    def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        result = len(wordsDict)
+        idx_1 = idx_2 = None
+        
+        for i, word in enumerate(wordsDict):
+            if word == word1:
+                idx_1 = i
+                if idx_2:
+                    result = min(result, idx_1 - idx_2)
+            if word == word2:
+                idx_2 = i
+                if idx_1 and idx_1 != idx_2:
+                    result = min(result, idx_2 - idx_1)
+        
+        return result
