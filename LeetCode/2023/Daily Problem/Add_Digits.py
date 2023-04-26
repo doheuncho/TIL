dce40278f4d582f5b53b9ad1a2ddde2749b59706
doheuncho <dh30594@gmail.com>
2023-04-26 21:43:51 +0900

    Self study: LeetCode 258

---
+++ LeetCode/2023/Daily Problem/Add_Digits.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/add-digits/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            num = sum([int(n) for n in str(num)])
+        
+        return num
+    
