3ce0d5b1bee7054c32f653a7822e9f841930dee5
doheuncho <dh30594@gmail.com>
2023-05-03 23:52:39 +0900

    Self study: LeetCode 2215

---
+++ LeetCode/2023/Daily Problem/Find_the_Difference_of_Two_Arrays.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set_1, set_2 = set(nums1), set(nums2)
+
+        return [list(set_1 - set_2), list(set_2 - set_1)]
+    
