15054c3f1fdd399093a50b751e9f235f17b45c03
doheuncho <dh30594@gmail.com>
2023-06-06 22:37:54 +0900

    Self study: LeetCode 1502

---
+++ LeetCode/2023/Daily Problem/Can_Make_Arithmetic_Progression_From_Sequence.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+
+        for i in range(len(arr)-2):
+            if arr[i+2] - arr[i+1] == arr[i+1] - arr[i]:
+                continue
+            else:
+                return False
+
+        return True
+        
